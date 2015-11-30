767ac7e60434c40d6e8cf15956fd0796d37936ee
hancong05 <hancong05@meituan.com>
2015-11-30 10:59:47 +0800

    update
    Signed-off-by: hancong05 <hancong05@meituan.com>

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -11,31 +11,33 @@ jQuery(function ($) {
     var $welcomeBg = $('.welcome-bg');
     var pageWidth;
 
-    //resize();
-    //
-    //function resize() {
-    //    pageWidth = document.documentElement.clientWidth;
-    //
-    //    var imageInfo = getImageWidth();
-    //
-    //    $welcomeBg.css('width', imageInfo.width);
-    //}
-    //
-    //
-    //function getImageWidth() {
-    //    var diff = IMAGE_WIDTH - pageWidth;
-    //    var imageWidth = IMAGE_WIDTH;
-    //
-    //    if (diff < OFFSET) {
-    //        imageWidth = pageWidth + OFFSET;
-    //        diff = imageWidth - pageWidth;
-    //    }
-    //
-    //    return {
-    //        width: imageWidth,
-    //        diff: diff
-    //    };
-    //}
-    //
-    //$(window).on('resize', resize);
+    var $welcomeAnimationImage = $('#welcomeAnimationImage');
+    var $finallImage = $($welcomeAnimationImage[0].cloneNode(true));
+
+    $finallImage[0].setAttribute('class', 'welcome-image finall-welcome-image')
+
+    $('.welcome-text').append($finallImage);
+
+    var animation = new Vivus('welcomeAnimationImage', {
+            type: 'delayed',
+            duration: 300,
+            start: 'manual',
+            dashGap: 0,
+            pathTimingFunction: Vivus.LINEAR
+        },
+        function () {
+            $finallImage.css({
+                opacity: 1
+            });
+
+            $welcomeAnimationImage.css({
+                opacity: 0
+            });
+
+            $('.entry-btn').addClass('show');
+        });
+
+    window.setTimeout(function () {
+        animation.play();
+    }, 2000);
 });
